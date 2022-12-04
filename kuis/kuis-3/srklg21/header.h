27c711efc85d8f7a9719b54b78fdf1a803d6e21f
Ananda Myzza Marhelio <anandamyzza13@gmail.com>
2022-12-04 16:12:35 +0700

    Header srklg21

---
+++ kuis/kuis-3/srklg21/header.h
@@ -0,0 +1,11 @@
+/*Saya Ananda Myzza Marhelio NIM 2100702 mengerjakan evaluasi KUIS 3 dalam mata kuliah Algoritma dan Pemrograman 1 
+untuk keberkahanNya maka saya tidak melakukan kecurangan seperti yang telah dispesifikasikan. Aamiin.*/
+
+#include <stdio.h>
+#include <string.h>
+
+/* Integer global untuk membantu proses for dalam program */
+int n, i, j, k;
+
+// Prosedur untuk proses seluruh program.
+void sentence(char main[][51]);
