0a35399893a18adcc17e34242d6d729d8e7a7085
Ananda Myzza Marhelio <anandamyzza13@gmail.com>
2022-12-04 15:42:25 +0700

    Header tbintp21

---
+++ uas/tbintp21/header.h
@@ -0,0 +1,16 @@
+/*Saya Ananda Myzza Marhelio NIM 2100702 mengerjakan UAS dalam mata kuliah Algoritma dan Pemrograman 1 
+untuk keberkahanNya maka saya tidak melakukan kecurangan seperti yang telah dispesifikasikan. Aamiin.*/
+
+#include <stdio.h>
+#include <string.h>
+
+/* Bungkusan untuk 4 nilai yang di input user */
+typedef struct{
+    int mark[4];
+}data;
+
+/* Integer global untuk membantu proses for dalam program */
+int n, i, j, k, l;
+
+// Prosedur untuk proses seluruh program.
+void amount(data value[]);
