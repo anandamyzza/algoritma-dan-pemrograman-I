b504c0cb41310c6bd1cf131691de171f57239568
Ananda Myzza Marhelio <anandamyzza13@gmail.com>
2022-12-04 15:44:32 +0700

    Header evaluas

---
+++ uas/evaluasi/header.h
@@ -0,0 +1,11 @@
+/*Saya Ananda Myzza Marhelio NIM 2100702 mengerjakan Evaluasi 2 dalam mata kuliah Algoritma dan Pemrograman 1 
+untuk keberkahanNya maka saya tidak melakukan kecurangan seperti yang telah dispesifikasikan. Aamiin.*/
+
+#include <stdio.h>
+#include <string.h>
+
+/* Integer global untuk membantu proses for dalam program */
+int n, i, j, k;
+
+// Prosedur untuk proses seluruh program.
+void divide(char str[][200], int jam, int menit, int detik);
