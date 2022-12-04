56dbe04f3cf677a867e91fde3c5730395aceadeb
Ananda Myzza Marhelio <anandamyzza13@gmail.com>
2022-12-04 15:40:53 +0700

    Header skktas21

---
+++ uas/skktas21/header.h
@@ -0,0 +1,11 @@
+/*Saya Ananda Myzza Marhelio NIM 2100702 mengerjakan UAS dalam mata kuliah Algoritma dan Pemrograman 1 
+untuk keberkahanNya maka saya tidak melakukan kecurangan seperti yang telah dispesifikasikan. Aamiin.*/
+
+#include <stdio.h>
+#include <string.h>
+
+/* Integer global untuk membantu proses for dalam program */
+int n, i, j, k, l;
+
+// Prosedur untuk proses seluruh program.
+int amount(char main[][100], char seek[]);
